b4a5cbdeaaa83050f2fc4f40140aaa879de38093
Liam B <71141670+liambambery23@users.noreply.github.com>
2021-03-03 11:31:41 -0500

    added component for search results

---
+++ src/components/SearchResults/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import "./style.css";
+
+function SearchResults(props) {
+  return (
+    <ul className="list-group search-results">
+        <span onClick={() => props.sortByName()} className="remove">
+       Sort Name
+      </span>
+      {props.results.map(result => (
+        <li key={result.email} className="list-group-item">
+          <img alt="Dog" src={result.picture.medium} className="img-fluid" />
+        {result.name.first} {result.name.last}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default SearchResults;
